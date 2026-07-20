232a5f2510f2e55a69cb43e60642c6b3ff61cb22
Ascensus <Ascensus@DESKTOP-VVIBNC7>
2026-07-20 14:27:54 +0200

    Добавьте файлы проекта.

---
+++ TodoApp.Domain/Entities/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.Domain.Entities
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset? UpdatedAt { get; set; }
+        public DateTimeOffset? DueDate { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public int? CategoryId { get; set; }
+
+        public Category? Category { get; set; } //Nav property to Category entity
+    }
+}
